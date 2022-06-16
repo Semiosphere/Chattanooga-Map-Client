10a32831ed0ac8b98a7938dac8801a6f978dd1ef
Zach Dugger <ztd522@gmail.com>
2022-06-16 15:31:20 -0400

    map page functioning per mvp

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -5,17 +5,17 @@
 //A user can click on the flag icon to place a flag on the map
 //A user can click their profile picture to route to their profile
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { Map, Marker, Tooltip } from 'react-canvas-map'
+import { Map, Marker } from 'react-canvas-map'
 import "./Map.css"
 
 
 export const MapForm = () => {
+  
   const markerImage = new Image()
   markerImage.src = `https://res.cloudinary.com/dvdug0mzg/image/upload/v1655240156/Chattanooga%20Map/Transparent_Marker-svg_xfl7po.svg`
 
-  const locationInfoBox = useRef()
 
   //The following code assigns click events to specific coordinates
   const [ markers, setMarkers ] = useState([]);
@@ -29,29 +29,35 @@ export const MapForm = () => {
       });
     }, [] );
 
-  
+
 
   const [activeMarker, setActiveMarker] = useState({})
   return (
     <>
-      <h1 className="MapHeaderText">Explore the map and try clicking on anything interesting!</h1>
-      <div className="MapContainer" style={{ height: '100vh', border: '1px solid #ddd', marginTop: '1rem' }}>
 
-        
-          <dialog className="info-box" ref={locationInfoBox} onClick={() => locationInfoBox.current.close()}>
-            <h1 id="location-name">{activeMarker.name}</h1>
-            <h2 id="location-coordinates">{activeMarker.coordinates}</h2>
-            <p id="location-description">{activeMarker.description}</p>
-            <img
-              id="info-art" src={activeMarker.character_art}
-            ></img>
-          </dialog>
-        
+      <div className="MapContainer" style={{ height: '100vh'}}>
 
+        <div className="cmc-logo-container">
+          <img id="cmc-logo" src="https://res.cloudinary.com/dvdug0mzg/image/upload/v1655402702/Chattanooga%20Map/CMC_text_mxyiqp.png"></img>
+        </div>
+
+        <div className="top-banner" id="map-banner">
+          <h1 id="MapHeaderText">Explore the map and try clicking on anything interesting!</h1>
+        </div>
+        
+        <div className={`info-box ${activeMarker.name?"show":"hide"}`} id="infoBox">
+          <h1 id="location-name">{activeMarker.name}</h1>
+          <h2 id="location-coordinates">{activeMarker.coordinates}</h2>
+          <p id="location-description">{activeMarker.description}</p>
+          <img
+            id="location-art" src={activeMarker.character_art}
+          ></img>
+        </div>
+        
         <Map
           className="MapImage"
           image="https://res.cloudinary.com/dvdug0mzg/image/upload/v1655239993/Chattanooga%20Map/Map_test_2_f5hnxu.png"
-          onClick={() => { setActiveMarker({}); }}
+          onClick={() => { setActiveMarker({})}}
           minZoom={1.1}
           maxZoom={0}
           overPan={0}
@@ -66,8 +72,6 @@ export const MapForm = () => {
                     image={markerImage}
                     onClick={() => {
                       setActiveMarker(marker)
-                      setTimeout(locationInfoBox.current.showModal(), 500)
-
                     }}
                     markerIndex={markerIndex}
                   />
